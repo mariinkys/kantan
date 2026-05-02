3ce5f5d00bb94d0d932f36c1b4ff231a5b0cd1d2
mariinkys <kysgeneral.nrgua@slmail.me>
2026-05-02 22:37:12 +0200

    chore: change extended icons for core icons (for now)
    Signed-off-by: mariinkys <kysgeneral.nrgua@slmail.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.material.icons.core)
     debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.androidx.navigation.compose)
