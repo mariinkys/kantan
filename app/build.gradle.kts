6d90c5bff7e5bb6ab2001aaa4f8e63519ae9cd19
mariinkys <kysgeneral.nrgua@slmail.me>
2026-05-04 14:53:59 +0200

    feat: new loading screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
